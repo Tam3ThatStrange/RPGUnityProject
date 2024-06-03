9b7794ffcfac5fe410b6382dc0037c6e8e050d35
Tam3ThatStrange <pussehl2010@yahoo.com>
2024-06-03 12:26:08 -0400

    Death animation
    Implmented the death animation

--- RPGproject/Assets/__Scripts/Combat/Health.cs
+++ RPGproject/Assets/__Scripts/Combat/Health.cs
@@ -5,12 +5,28 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
 
+        private bool bIsDead = false;
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+          
+
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+          
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (bIsDead) return;
+            
+
+            bIsDead = true;
+            GetComponent<Animator>().SetTrigger("Die");
         }
     }
 }
